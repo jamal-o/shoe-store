6ebdb01dff0759aeae2ed0163aa47957a5485c52
jamal-o <jamaldeenolasupo@gmail.com>
2024-08-08 15:30:11 +0100

    add documentation

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import express from 'express';
 import 'dotenv/config';
 
 
+import apiDocConfigjson from './config/openapi.json' assert {type: 'json'};
+
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 import productRouter from './routes/product.route.js';
 
 import authRouter from './routes/auth.route.js';
@@ -34,7 +39,52 @@ app.use('/api/v1/auth', authRouter);
 
 app.use('/api/v1/purchase', purchaseRouter)
 
-app.use('*', (req, res) => {
+
+
+
+
+const options = {
+    definition: apiDocConfigjson,
+    apis: ["./routes/*.js"],
+  };
+  
+//   const options = {
+//     definition: {
+//       openapi: "3.1.0",
+//       info: {
+//         title: "Barb shoe store Express API with Swagger",
+//         version: "0.1.0",
+//         description:
+//           "This is an e-commerce API application made with Express and documented with Swagger",
+//         license: {
+//           name: "MIT",
+//           url: "https://spdx.org/licenses/MIT.html",
+//         },
+//         contact: {
+//           name: "Admin",
+//           url: "https://barbstore.com",
+//           email: "admin@barbstore.com",
+//         },
+//       },
+//       servers: [
+//         {
+//           url: `http://localhost:${PORT}`,
+//         },
+//       ],
+//     },
+//     apis: ["./routes/*.js"],
+//   };
+  
+  const specs = swaggerJsdoc(options);
+
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, {explorer: true})
+  );
+
+
+  app.use('*', (req, res) => {
     res.status(404).json({message: 'not found'});
 });
 
