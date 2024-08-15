abf9ea527528ef8fb0362aaa3d147ce1916c0a37
jamal-o <jamaldeenolasupo@gmail.com>
2024-08-15 11:06:55 +0100

    add validator to auth route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,10 +5,11 @@ import { signup,
     signout,
 } from '../controllers/auth.controller.js';
 
+import { jsonValidator, userSignupSchema } from '../middlewares/validators.js';
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post('/signup', jsonValidator({body: userSignupSchema}), signup);
+router.post('/signin', jsonValidator({body: userSignupSchema}), signin);
 router.get('/signout', signout);
 
 export default router;
